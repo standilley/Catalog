74d1582b61d4811a7f9487852049fae4b9f01483
stand.personal <stand@DESKTOP-1PVOLSM>
2025-11-05 23:01:26 -0300

    feat(domain): Add Produto entity, domain intercafes and exceptions

---
+++ src/Backend/Catalog.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message)
+            : base(message) { }
+    }
+}
---
+++ src/Backend/Catalog.Domain/Interfaces/IRepositoryProduto.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.Domain.Entities;
+namespace Catalog.Domain.Interfaces
+{
+    public interface IRepositoryProduto
+    {
+        Task<Produto> ObterPorIdAsync(Guid id);
+        Task<IEnumerable<Produto>> ObterTodosAsync();
+        Task AdicionarAsync(Produto produto);
+        Task AtualizarAsync(Produto produto);
+        Task RemoverAsync(Guid id);
+    }
+}
